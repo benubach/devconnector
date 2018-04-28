4fb793a5393c2b8337ebf46ab9ba0c8cf31649af
Benjamín Ubach <benubach@gmail.com>
2018-04-28 00:55:08 -0500

    Added mocha/chai/sinon support
    Moved all code to src/
    Added initial test :D

--- config/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://ubach:ubach@ds041536.mlab.com:41536/devconnector'
-};
---
+++ src/config/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://devconnectadmin:devconnectadmin@ds135252.mlab.com:35252/devconnect'
+};
